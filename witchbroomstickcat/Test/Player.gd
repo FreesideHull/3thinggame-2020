bde3214bfb268dcb01010ffb439b988ce32b1a84
Christopher Boczko <Chris@furtherthinking.net>
2020-10-31 12:06:13 +0000

    add music

--- witchbroomstickcat/Test/Player.gd
+++ witchbroomstickcat/Test/Player.gd
@@ -3,15 +3,13 @@ extends KinematicBody2D
 var speed = 200
 var facing_right = true
 var velocity = Vector2()
-var bossfight = false
 
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-	
 
 func get_input():
 	velocity = Vector2()
@@ -20,13 +18,9 @@ func get_input():
 	else:
 		$Sprite.scale.x = -1
 	
-	if bossfight != true:
-		velocity.x += 1
-	
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 		$AnimationPlayer.play("Run")
-	
 	elif Input.is_action_pressed("down"):
 		velocity.y += 1
 		$AnimationPlayer.play("Run")
