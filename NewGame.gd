9afa64a4b9cb1eee12afdc1ea8c95f3f4b0e1594
Christopher Boczko <chris@furtherthinking.net>
2020-10-31 13:59:02 +0000

    added credits / game over screens

--- NewGame.gd
+++ NewGame.gd
@@ -3,12 +3,17 @@ extends Control
 
 var scene
 
-
+func _on_creditsButton_pressed():
+	scene = "res://witchbroomstickcat/Credits/Credits.tscn"
+	$FadeIn.show()
+	$FadeIn.fade_in()
 
 func _on_StartGameButton_pressed():
 	scene = "res://witchbroomstickcat/Test/World.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in()
+	
+
 
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene)
