c1aced012fd3bbca5ea1e99dcf353f1d1f73efad
NATHAN DOWNES <nathandownes191@outlook.com>
2020-10-31 14:54:21 +0000

    Added fireball collision and half bat collision

--- witchbroomstickcat/Test/Enemy.gd
+++ witchbroomstickcat/Test/Enemy.gd
@@ -11,11 +11,15 @@ func _ready():
 func dead():
 	is_dead = true
 	velocity = Vector2(0, 0)
+	$CollisionShape2D.disabled = true
 	$AnimatedSprite.play("dead")
+	$Timer.start()
 
 func _physics_process(_delta):
 	if is_dead == false:
 		velocity.x -= speed
 		$AnimatedSprite.play("move")
-	
 		velocity = move_and_slide(velocity)	
+
+func _on_Timer_timeout():
+	queue_free()
